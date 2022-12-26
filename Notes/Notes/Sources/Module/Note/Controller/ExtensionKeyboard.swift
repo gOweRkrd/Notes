ac0587241aee367b2c460f694bb6fba042339ce3
Alexander Kosyakov <110721351+gOweRkrd@users.noreply.github.com>
2022-12-26 16:43:07 +0400

    Refactored

--- Notes/Notes/Sources/Module/Note/Controller/ExtensionKeyboard.swift
+++ Notes/Notes/Sources/Module/Note/Controller/ExtensionKeyboard.swift
@@ -12,37 +12,32 @@ extension UITextView {
     
     func addButtonKeyboard(Bold: (target: Any, action: Selector)? = nil,
                            Italic: (target: Any, action: Selector)? = nil,
-                           Underline: (target: Any, action: Selector)? = nil,
-                           Font:(target: Any, action: Selector)? = nil,
-                           Image:(target: Any, action: Selector)? = nil) {
+                           Underline: (target: Any, action: Selector)? = nil)
+    {
         
         let Bold = Bold ?? (target: self, action: #selector(boldlButtonTapped))
         let Italic = Italic ?? (target: self, action: #selector(italicButtonTapped))
         let Underline = Underline ?? (target: self, action: #selector(underlineButtonTapped))
-        let Font = Font ?? (target: self, action: #selector(fontButtonTapped))
-        let Image = Image ?? (target: self, action: #selector(imageButtonTapped))
         
         let toolbar : UIToolbar = UIToolbar()
         toolbar.barStyle = .default
         toolbar.items = [
             UIBarButtonItem(title: "Bold", style: .plain, target: Bold.target, action: Bold.action),
             UIBarButtonItem(title: "Italic", style: .done, target: Italic.target, action: Italic.action),
-            UIBarButtonItem(title: "Underline", style: .done, target: Underline.target, action: Underline.action),
-            UIBarButtonItem(title: "Font", style: .done, target: Font.target, action: Font.action),
-            UIBarButtonItem(title: "Image", style: .done, target: Image.target, action: Image.action)
+            UIBarButtonItem(title: "Underline", style: .done, target: Underline.target, action: Underline.action)
         ]
         toolbar.sizeToFit()
         self.inputAccessoryView = toolbar
     }
     // MARK: - Private Method
     
-   private  func attributedString(highlightedTextStyle: [NSAttributedString.Key: Any]) -> Void {
+    private  func attributedString(highlightedTextStyle: [NSAttributedString.Key: Any]) -> Void {
         let selfText = self.attributedText
-
+        
         let formattedString = NSMutableAttributedString(attributedString: selfText!)
         
         let range = self.selectedRange
-
+        
         formattedString.setAttributes(highlightedTextStyle, range: range)
         self.attributedText = formattedString
     }
@@ -69,16 +64,6 @@ extension UITextView {
         self.attributedString(highlightedTextStyle: [.underlineStyle: NSUnderlineStyle.thick.rawValue, .font: UIFont.systemFont(ofSize: 20)])
         
     }
-    @objc func fontButtonTapped() {
-        self.resignFirstResponder()
-        
-    }
-    
-    @objc func imageButtonTapped() {
-        self.resignFirstResponder()
-        
-    }
-    
 }
 
 
